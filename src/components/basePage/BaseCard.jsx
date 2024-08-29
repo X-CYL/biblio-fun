a2e32d205c58c264eb3c8c33d2e400d39f61352b
X-CYL <castellastro@gmail.com>
2024-08-29 13:08:30 +0200

    ending to build the card, maybe some more touch-ups later

--- src/components/basePage/BaseCard.jsx
+++ src/components/basePage/BaseCard.jsx
@@ -6,12 +6,12 @@ export default function BaseCard() {
     <>
       <div className="grid h-screen place-items-center ">
         <div className="md:bg-bg-secondary w-72 md:w-[700px] xl:w-[1200px] rounded-md h-[600px] ">
-          <h2 className="text-4xl md:text-6xl text-center font-primary-font text-bg-secondary md:text-bg-primary ">
-            Les derniers livres enregistrées
+          <h2 className="text-4xl md:text-6xl text-center mt-[-30px] md:mt-2 font-primary-font text-bg-secondary md:text-bg-primary ">
+            Les derniers livres enregistrés
           </h2>
           <body>
             <div>
-              <div className="flex flex-wrap justify-center scrollbar scrollbar-thumb-nav-text-color-selected scrollbar-track-sky-300 h-[400px] gap-3 overflow-y-scroll lg:gap-3 md:gap-3 md:h-[490px]">
+              <div className="flex flex-wrap md:mt-4 justify-center scrollbar scrollbar-thumb-nav-text-color-selected scrollbar-track-sky-300 h-[400px] gap-3 overflow-y-scroll lg:gap-3 md:gap-3 md:h-[490px]">
                 <Card />
                 <Card />
                 <Card />
--- src/components/micro/card.jsx
+++ src/components/micro/card.jsx
@@ -3,46 +3,41 @@ import pics1 from "../../assets/pics/la_derniere_chasse.png";
 
 export default function Card() {
   return (
-    <>
-      <div className="">
-        <div className="flex bg-bg-card w-72 h-40 sm:w-[400px] md:w-[500px] xl:w-[500px] rounded-xl  border-4 border-black ">
-          <div>
+      <>
+        <div className="">
+          <div className="flex bg-bg-card w-72 h-40 sm:w-[400px] md:w-[500px] xl:w-[500px] rounded-xl  border-4 border-black ">
             <img
-              className="h-16 w-28 mt-2 ml-2"
+              className="h-16 w-28 md:w-16 mt-2 ml-2"
               src={pics1}
               alt="couverture la derniere chasse de grangé"
             />
-          </div>
-          <div>
-            <p className="ml-2 mt-1 font-card-font text-sm">
-              Titre : La derniere chasse
-            </p>
-            <p className="ml-2 font-card-font text-sm">
-              Auteur: Jean Christophe Grangé
-            </p>
-            <div className="flex">
+            <div>
+              <p className="ml-2 mt-1 font-card-font text-sm">
+                Titre : La derniere chasse
+              </p>
+              <p className="ml-2 font-card-font text-sm">
+                Auteur: Jean Christophe Grangé
+              </p>
               <p className="ml-2 font-card-font text-sm">Pitch: </p>
               <p className="ml-1 mt-1 font-card-font text-xs">
                 Un cadavre est retrouvé dans une clairière, que fait-il la au
                 milieu de cette forêt appartenant à cette riche famille?{" "}
               </p>
             </div>
-            <div className="flex mt-3 justify-between">
-          <div className="flex mr-50">
-            <p className="ml-1 mt-1 font-card-font text-xs">Publié par :</p>
-            <p className="font-card-font text-x">X-CYL</p>
           </div>
-          <div className="flex">
-            <p className="ml-1 mt-1 font-card-font text-xs">Likes :</p>
-            <p className="font-card-font mt-1 ml-1 text-xs text-yellow-300">
-              12
-            </p>
+          <div className="flex justify-between mt-[-30px] ml-6 mr-6 md:ml-8 md:mr-12]">
+            <div className="flex">
+              <p className="ml-1 mt-1 font-card-font text-xs">Publié par :</p>
+              <p className="font-card-font text-yellow-300">X-CYL</p>
+            </div>
+            <div className="flex">
+              <p className="ml-1 mt-1 font-card-font text-xs">Likes :</p>
+              <p className="font-card-font mt-1 ml-1 text-xs text-red-600">
+                45
+              </p>
+            </div>
           </div>
         </div>
-          </div>
-        </div>
-        
-      </div>
-    </>
-  );
+      </>
+    );
 }
